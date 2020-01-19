62f8c8e0fe7355ace1039afa30148f598f50a37f
rtning <rtning@thoughtworks.com>
2020-01-19 14:45:40 +0800

    refactor car executor test

--- src/test/java/tdd/practice/car/CarExecutorTest.java
+++ src/test/java/tdd/practice/car/CarExecutorTest.java
@@ -9,19 +9,29 @@ import static org.assertj.core.api.Assertions.assertThat;
     1.car 1,1 N cmd M -> 1,2 N
     2.car 1,1 N cmd L -> 1,1 W
     3.car 1,1 N cmd R -> 1,1 E
-    4.car 1,1 N cmd L M R -> mock .. -> 0,1 N
+    4.car 1,1 N cmd L M R  -> 0,1 N
+    5.car 1,1 N cmd B M M -> 1，-1 N
  */
 
 public class CarExecutorTest {
     @Test
-    public void print_car_location_when_send_cmd_move() {
-        Coordinate coordinate = new Coordinate(1,1);
-        Car car = new Car(coordinate, Direction.NORTH);
-
+    public void execute_car_when_send_cmd_L_M_R() {
+        Car car = initialCarExecutor(1,1, Direction.NORTH);
         CarExecutor carExecutor = new CarExecutor(car);
+
         carExecutor.execute("L M R");
-        assertThat(car.getCoordinate().getX()).isEqualTo(0);
-        assertThat(car.getCoordinate().getY()).isEqualTo(1);
-        assertThat(car.getDirection()).isEqualTo(Direction.NORTH);
+
+        assertCarLocation(car, 0, 1, Direction.NORTH);
+    }
+
+    private void assertCarLocation(Car car, int x, int y, Direction d) {
+        assertThat(car.getCoordinate().getX()).isEqualTo(x);
+        assertThat(car.getCoordinate().getY()).isEqualTo(y);
+        assertThat(car.getDirection()).isEqualTo(d);
+    }
+
+    private Car initialCarExecutor(int x, int y, Direction d) {
+        Coordinate coordinate = new Coordinate(x, y);
+        return new Car(coordinate, d);
     }
 }
